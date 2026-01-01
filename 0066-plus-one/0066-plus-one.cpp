eff9391b4c8d10f5f281513a6b1597e7b788e55d
Rahul Saha <97524553+Rahul99Saha@users.noreply.github.com>
2026-01-01 18:08:15 +0530

    Time: 1 ms (8.18%), Space: 11.6 MB (14.65%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,19 @@
+//8999
+//9->10
+//9000
+//carry propagation
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i = digits.size()-1;i>=0;i--){
+            digits[i]+=1;
+            if(digits[i]!=10)
+            {
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
