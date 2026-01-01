005bea66c8f85b025534b289fdfe3a0805e92089
Rahul Saha <97524553+Rahul99Saha@users.noreply.github.com>
2026-01-01 18:09:29 +0530

    Time: 0 ms (100%), Space: 11.5 MB (50.17%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -7,7 +7,7 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         for(int i = digits.size()-1;i>=0;i--){
             digits[i]+=1;
-            if(digits[i]!=10)
+            if(digits[i]<=9)
             {
                 return digits;
             }
