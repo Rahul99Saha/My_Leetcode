e6db4e43944ef72bd5b17b0c6be34abd1639c4a6
Rahul Saha <97524553+Rahul99Saha@users.noreply.github.com>
2026-03-04 12:07:16 +0530

    Time: 25 ms (18.12%), Space: 29 MB (55.39%) - LeetHub

--- 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.cpp
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.cpp
@@ -3,27 +3,27 @@ public:
     vector<string> validStrings(int n) 
     {
         vector<string> ans;
-        vector<int> ds;
+        vector<string> ds;
         generate_subsequence(0, n, ds, ans);
         return ans;
     }
-    void generate_subsequence(int index,int n,vector<int>&ds,vector<string>&ans)
+    void generate_subsequence(int index,int n,vector<string>&ds,vector<string>&ans)
     {
         if(index == n)
         {   
             string temp = "";
             for(int i= 0;i<ds.size();i++){    
-                temp += to_string(ds[i]);
+                temp += ds[i];
             }
             ans.push_back(temp);
             return;
         }
-        ds.push_back(1);
+        ds.push_back("1");
         generate_subsequence(index+1, n,ds,ans);
         ds.pop_back();
 
-        if(ds.empty() || ds.back() != 0){
-            ds.push_back(0);
+        if(ds.empty() || ds.back() != "0"){
+            ds.push_back("0");
             generate_subsequence(index+1, n,ds,ans);
             ds.pop_back();
         }
