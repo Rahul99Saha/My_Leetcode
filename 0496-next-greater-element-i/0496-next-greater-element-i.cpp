a457e00f0fe4f85f73828467927cc9b42753764f
Rahul Saha <97524553+Rahul99Saha@users.noreply.github.com>
2026-06-04 12:10:59 +0530

    Time: 2 ms (47.03%), Space: 12.8 MB (60.75%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -4,24 +4,22 @@ public:
         //nums1 = [4,1,2], nums2 = [1,3,4,2]
         //Output: [-1,3,-1]
         vector<int>store;
+        unordered_map<int,int>mp;
         int n =nums1.size();
         int m = nums2.size();
         int j=0;
-        int greater = -1;
-        for(int i =0;i<n;i++){
-            while(nums2[j]!=nums1[i] && j<m-1)
-            {
-                j++;
-            }
-            for(int k=j;k<m;k++){
-                if(nums2[k]>nums1[i]){
-                    greater = nums2[k];
+        for(int i =0;i<m;i++){
+            mp[nums2[i]] = -1;
+            for(int j=i;j<m;j++){
+                if(nums2[i]<nums2[j]){
+                    mp[nums2[i]] = nums2[j];
                     break;
                 }
             }
-            store.push_back(greater);
-            greater = -1;
-            j=0;
+        }
+
+        for(int i = 0;i<n;i++){
+            store.push_back(mp[nums1[i]]);
         }
         return store;
     }
