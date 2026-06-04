70b57bdc4d5de019c4291ec794fab21742491bb0
Rahul Saha <97524553+Rahul99Saha@users.noreply.github.com>
2026-06-04 14:07:46 +0530

    Time: 0 ms (100%), Space: 12.9 MB (36.84%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        //nums1 = [4,1,2], nums2 = [1,3,4,2]
-        //Output: [-1,3,-1]
+        //nums1 = [4,1,2], nums2 = [1,3,4,2].  2
+        //Output: [-1,3,-1]t
+        stack<int>st;
         vector<int>store;
         unordered_map<int,int>mp;
         int n =nums1.size();
         int m = nums2.size();
         int j=0;
-        for(int i =0;i<m;i++){
-            mp[nums2[i]] = -1;
-            for(int j=i;j<m;j++){
-                if(nums2[i]<nums2[j]){
-                    mp[nums2[i]] = nums2[j];
-                    break;
-                }
-            }
+        for(int i =m-1;i>=0;i--){
+            while(!st.empty() && st.top()<nums2[i])
+            {
+                st.pop();
+            } 
+            if(st.empty())
+                mp[nums2[i]]= -1;
+            else
+                mp[nums2[i]]= st.top();
+            st.push(nums2[i]);
         }
 
         for(int i = 0;i<n;i++){
+
             store.push_back(mp[nums1[i]]);
         }
         return store;
