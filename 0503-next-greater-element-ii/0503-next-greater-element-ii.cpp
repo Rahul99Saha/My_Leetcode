e40489184b7b6b44be38e0eb231ee44c199b7698
Rahul Saha <97524553+Rahul99Saha@users.noreply.github.com>
2026-06-04 17:59:07 +0530

    Time: 2 ms (78.72%), Space: 28.4 MB (60.47%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
-//[100,1,11,1,120,111,123,1,-1,-100]
-//[120,11,120,11,123,123,-1,11,100,100]
-//[120,11,120,120,123,123,-1,100,100,100]
-
     vector<int> nextGreaterElements(vector<int>& nums) {
         stack<int>st;
-        
-        unordered_map<int,int>mp;
         int n = nums.size();
-        vector<int>store(2*n,-1);
-        for(int i=0;i<n;i++){
-            nums.push_back(nums[i]);
-        }
+        vector<int>store(n,-1);
         for(int i =2*n-1;i>=0;i--){
-            while(!st.empty() && st.top()<=nums[i])
+            while(!st.empty() && st.top()<=nums[i%n])
             {
                 st.pop();
             } 
-            if(st.empty())
-                store[i]=-1;
-            else
+            if(i < n && !st.empty()){
                 store[i] = st.top();
-            st.push(nums[i]);
+            }
+            st.push(nums[i%n]);
         }
-        vector<int> ans(store.begin(), store.begin() + n);
-        return ans ;
+        return store;
     }
 };
