f9ff7660dffe65144ab474e00707cc03baf16e13
Rahul Saha <97524553+Rahul99Saha@users.noreply.github.com>
2026-06-08 13:54:49 +0530

    Time: 0 ms (100%), Space: 25.9 MB (92.77%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
 // [0,1,0,2,1,0,1,3,2,1,2,1]
-// [0,1,1,2,2,2,2,3,3,3.3.3]
-// [3 3 3 3 3 3 3 3 2 2 2 1]
-// [0 1 1 2 2 2 2 3 2 2 2 1]
-// [0,0,1,0,1,2,1,0,0,1,0,0] - sum = 6
-
-// [0,1,0,2,1,0,1,3,2,1,2,1]
 //leftMax = 2  righMax =2 - i=3,j=n-2
 // ans += min(leftMax,righMax) - nums(pos)
     int trap(vector<int>& height) {
         int ans = 0;
         int n =height.size();
-        int leftmax[n];
-        int rightmax[n];
-        leftmax[0]=height[0];
-        rightmax[n-1]=height[n-1];
-        for(int i = 1;i<n;i++){
-            leftmax[i] = max(height[i],leftmax[i-1]);
-        }
-        for(int i = n-2;i>=0;i--){
-            rightmax[i] = max(height[i],rightmax[i+1]);
-        }
-        for(int i = 0;i<n;i++){
-            ans+=min(leftmax[i],rightmax[i])-height[i];
+        int leftmax=0;
+        int rightmax=0;
+        int left = 0;
+        int right = n-1;
+        while(left<right)
+        {
+            leftmax = max(leftmax,height[left]);
+            rightmax = max(rightmax,height[right]);
+            if(leftmax<=rightmax){
+                ans +=min(leftmax,rightmax)-height[left];
+                left++;
+            }
+            else{
+                ans +=min(leftmax,rightmax)-height[right];
+                right--;
+            }
         }
         return ans;
     }
