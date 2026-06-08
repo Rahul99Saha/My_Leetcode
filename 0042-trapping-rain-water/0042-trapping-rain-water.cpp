3d28c1e5a2416b2c292b48a099865fbc0dec8bfe
Rahul Saha <97524553+Rahul99Saha@users.noreply.github.com>
2026-06-08 13:07:45 +0530

    Time: 0 ms (100%), Space: 26 MB (55.15%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-//O(n^2) complexity
-// using namespace std;
-    int trap(vector<int>& height) 
-    {
-        int ans=0;
-        int n=height.size();
-        for(int i=1;i<n-1;i++)
-        {
-            int max_left=height[i];
-            for(int j=0;j<i;j++)
-            {
-                max_left=max(max_left,height[j]);
-            }
-            int max_right=height[i];
-            for(int j=i;j<n;j++)
-            {
-                max_right=max(max_right,height[j]);
-            }
-            ans=ans+min(max_right,max_left)-height[i];
+// [0,1,0,2,1,0,1,3,2,1,2,1]
+// [0,1,1,2,2,2,2,3,3,3.3.3]
+// [3 3 3 3 3 3 3 3 2 2 2 1]
+// [0 1 1 2 2 2 2 3 2 2 2 1]
+// [0,0,1,0,1,2,1,0,0,1,0,0] - sum = 6
+
+// [0,1,0,2,1,0,1,3,2,1,2,1]
+//leftMax = 2  righMax =2 - i=3,j=n-2
+// ans += min(leftMax,righMax) - nums(pos)
+    int trap(vector<int>& height) {
+        int ans = 0;
+        int n =height.size();
+        int leftmax[n];
+        int rightmax[n];
+        leftmax[0]=height[0];
+        rightmax[n-1]=height[n-1];
+        for(int i = 1;i<n;i++){
+            leftmax[i] = max(height[i],leftmax[i-1]);
         }
-        return ans;    
+        for(int i = n-2;i>=0;i--){
+            rightmax[i] = max(height[i],rightmax[i+1]);
+        }
+        for(int i = 0;i<n;i++){
+            ans+=min(leftmax[i],rightmax[i])-height[i];
+        }
+        return ans;
     }
 };
