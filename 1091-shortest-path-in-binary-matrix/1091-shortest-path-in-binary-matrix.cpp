6abb8a140d142d8d0d23ffd1400d2632a424ca75
Rahul Saha <97524553+Rahul99Saha@users.noreply.github.com>
2026-07-22 18:58:55 +0530

    Time: 13 ms (78.44%), Space: 25.3 MB (53.48%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,32 +2,32 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-
-        if (grid[0][0] == 1 || grid[n-1][n-1] == 1)
+        int dis = 0;
+        vector<vector<int>>visited(n,vector<int>(n,0));
+        if(grid[0][0]==1 || grid[n-1][n-1]==1)
             return -1;
-        
-        if(grid.size()==1) return 1;
-        vector<vector<int>>dist(n,vector<int>(n,1e9));
-        dist[0][0]=1;
-        queue<pair<int,pair<int,int>>>q;
-        q.push({1,{0,0}});
-
-        int dx[] = {-1,-1,0,1,1,1,0,-1};
-        int dy[] = {0,1,1,1,0,-1,-1,-1};
+        if(n==1) return 1;
+        visited[0][0] = 1;
+        queue<pair<int,int>>q;
+        q.push({0,0});
         while(!q.empty()){
-            int dis = q.front().first;
-            int r = q.front().second.first;
-            int c = q.front().second.second;
-            q.pop();
-
-            for(int i=0;i<8;i++){
-                int newr = r + dx[i];
-                int newc = c + dy[i];
-
-                if(newr>=0 && newc >= 0 && newr < n && newc < n && grid[newr][newc]==0 && dist[newr][newc] > dis +1){
-                    if(newr == n-1 && newc ==n-1) return dis+1;
-                    dist[newr][newc]= dis +1;
-                    q.push({dis+1,{newr,newc}});
+            int s = q.size();
+            dis++;
+            for(int i = 0;i<s;i++){
+                int x = q.front().first;
+                int y = q.front().second;
+                q.pop();
+                for(int row = -1;row<=1;row++){
+                    for(int col = -1;col<=1;col++){
+                        int nrow = x + row;
+                        int ncol = y + col;
+                        if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && !visited[nrow][ncol] && grid[nrow][ncol] == 0){
+                            visited[nrow][ncol] = 1;
+                            q.push({nrow,ncol});
+                            if(nrow ==n-1 && ncol == n-1) 
+                                return dis+1;
+                        }
+                    }
                 }
             }
         }
